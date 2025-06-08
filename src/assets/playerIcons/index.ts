b152672c3eb91e122c52dbe30de0175a17b634cc
TorrellesN <230282@fppiramide.com>
2025-06-08 20:38:19 +0200

    imgs

---
+++ src/assets/playerIcons/index.ts
@@ -0,0 +1,9 @@
+import catPlayerImg from './cat-player.png';
+import grullaPlayerImg from './grulla-player.png';
+import lunaPlayerImg from './luna-player.png';
+import makiPlayerImg from './maki-player.png';
+import ninjaPlayerImg from './ninja-player.png';
+import shurikenPlayerImg from './shuriken-player.png';
+import yinyangPlayerImg from './yinyan-player.png';
+
+export { catPlayerImg, grullaPlayerImg, lunaPlayerImg, makiPlayerImg, ninjaPlayerImg, shurikenPlayerImg, yinyangPlayerImg };
